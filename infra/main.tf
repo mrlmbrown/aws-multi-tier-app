27413fa0db82526848d86893f70f115eceace866
mrlmbrown <112828634+mrlmbrown@users.noreply.github.com>
2025-12-28 15:03:46 -0500

    Change EC2 instance type to t3.micro for Free Tier compatibility

--- infra/main.tf
+++ infra/main.tf
@@ -118,7 +118,7 @@ resource "aws_security_group" "web" {
 # EC2 Instance
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux_2.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.web.id]
 
